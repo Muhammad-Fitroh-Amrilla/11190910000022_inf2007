d86e011d90d956d5fe489fb15ccdfb54dd7b1d18
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2019-09-26 03:08:13 +0000

    Upload New File

---
+++ Praktek/Pertemuan/Tugas/HelloWorld.java
@@ -0,0 +1,13 @@
+	/*PROGRAM HelloWorld */
+	/* Penjelasan untuk mencatat "Hello World". Masukan program ini tidak 
+	ada. Keluarannya adalah tulisan 'Hello, world' tercetak di layar */
+
+	public class HelloWorld {
+		/* DEKLARASI */ 
+		/* tidak ada */
+
+		/* ALGORITMA */ 
+		public static void main (String[] args) {
+			System.out.print("Hello, world");
+		}
+	}
