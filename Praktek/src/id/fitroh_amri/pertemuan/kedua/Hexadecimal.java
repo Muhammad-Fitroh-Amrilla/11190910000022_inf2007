9793102ab76ee4cc1e4eef3cb0a69568c0aafb17
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2019-09-27 05:41:08 +0000

    Source code

---
+++ Praktek/src/id/fitroh_amri/pertemuan/kedua/Hexadecimal.java
@@ -0,0 +1,16 @@
+package id.fitroh_amri.pertemuan.kedua;
+
+/**
+ *
+ * @author FITROH_08
+ */
+public class Hexadecimal {
+    public static void main(String args[]){
+        int x = 0x0001;
+        int y = 0x7fffffff;
+        int z = 0xdeadcafe;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+}
