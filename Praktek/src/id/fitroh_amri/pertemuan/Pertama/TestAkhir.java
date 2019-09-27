35162b82621d868a910fdd6bf5eedbf1b8afce85
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2019-09-27 06:42:15 +0000

    Source code

---
+++ Praktek/src/id/fitroh_amri/pertemuan/Pertama/TestAkhir.java
@@ -0,0 +1,8 @@
+package id.fitroh_amri.pertemuan.pertama;
+
+public class TestAkhir {
+
+    public static void main(String[] args) {
+        System.out.print("What's wrong with this program?");
+    }
+}
