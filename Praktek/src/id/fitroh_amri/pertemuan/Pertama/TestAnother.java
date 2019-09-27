5153f915a09ff11a88382c870e9be1ef91edf787
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2019-09-27 06:42:40 +0000

    Source code

---
+++ Praktek/src/id/fitroh_amri/pertemuan/Pertama/TestAnother.java
@@ -0,0 +1,8 @@
+package id.fitroh_amri.pertemuan.pertama;
+
+public class TestAnother {
+
+    public static void main(String[] args) {
+        System.out.print("What's wrong with this program?");
+    }
+}
