0a197a8c3a9ddec18a4febf051dcc7871c336b13
Muhammad Fitroh Amrilla <amrillafitroh@gmail.com>
2019-09-27 05:39:28 +0000

    Source code

---
+++ Praktek/src/id/fitroh_amri/pertemuan/kedua/Assign.java
@@ -0,0 +1,26 @@
+package id.fitroh_amri.pertemuan.kedua;
+/**
+ *
+ * @author FITROH_08
+ */
+public class Assign {
+
+    public static void main(String args[]) {
+        boolean b = true;
+        System.out.println("Value b = " + b);
+        char c = 'C';
+        System.out.println("Value c = " + c);
+        byte bt = 10;
+        System.out.println("Value bt = " + bt);
+        short s = 20;
+        System.out.println("Value s = " + s);
+        int i = 30;
+        System.out.println("Value i = " + i);
+        long l = 40L;
+        System.out.println("Value l = " + l);
+        float f = 3.14F;
+        System.out.println("Value f = " + f);
+        double d = 3.14;
+        System.out.println("Value d = " + d);
+    }
+}
